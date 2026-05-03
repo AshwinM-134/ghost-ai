6bed9cbb275d3565e620f67c1fa5e8c0d4c0b0c5
Ashwin <ashwin.m134@gmail.com>
2026-05-03 16:33:23 +0530

    feat: add cached prisma client singleton with accelerate/pg branching

---
+++ lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "../app/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+function createPrismaClient(): PrismaClient {
+  const url = process.env.DATABASE_URL ?? "";
+
+  if (url.startsWith("prisma+postgres://")) {
+    return new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
+  }
+
+  const adapter = new PrismaPg({ connectionString: url });
+  return new PrismaClient({ adapter });
+}
+
+export const prisma: PrismaClient =
+  global.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
